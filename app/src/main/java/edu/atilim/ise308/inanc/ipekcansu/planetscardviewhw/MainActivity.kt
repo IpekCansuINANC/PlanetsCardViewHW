2e3b3019b7a4b1ec1d10c106330dde6f60217c0b
ipek270199 <icansuinanc@gmail.com>
2020-10-29 12:12:36 +0300

    Initial commit

---
+++ app/src/main/java/edu/atilim/ise308/inanc/ipekcansu/planetscardviewhw/MainActivity.kt
@@ -0,0 +1,13 @@
+package edu.atilim.ise308.inanc.ipekcansu.planetscardviewhw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+    }
+}
